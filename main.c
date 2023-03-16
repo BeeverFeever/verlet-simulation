b4b172ac4b419b202bbc87c6aaad59ec3fea471a
BeeverFeever <scottyadam6@gmail.com>
2023-03-16 14:29:14 +1100

    Acctually implementing verlet integration rather than Euler

--- main.c
+++ main.c
@@ -4,8 +4,9 @@
 typedef unsigned int uint;
 
 typedef struct {
-    Vector2 position;
-    Vector2 velocity;
+    Vector2 cur_pos;
+    Vector2 last_pos;
+    Vector2 acceleration;
     uint radius;
 } circle_t;
 
@@ -13,7 +14,7 @@ typedef struct {
 float dt; // delta time
 const int screenWidth = 800;
 const int screenHeight = 450;
-const float gravity = 9.8f;
+const Vector2 gravity = { 0.0f, 980.0f };
 
 void render_circles(int count, circle_t circles[count]) {
     for (int i = 0; i < count; i++) {
@@ -21,25 +22,44 @@ void render_circles(int count, circle_t circles[count]) {
     }
 }
 
-void update_circles(int count, circle_t* circles) {
-    for (int i = 0; i < count; i++) {
-        circles[i].position.y += circles[i].velocity.y*gravity*dt;
-        circles[i].position.x += circles[i].velocity.x*dt;
-        if ((circles[i].position.y + circles[i].radius) > screenHeight) {
-            circles[i].position.y = screenHeight - circles[i].radius;
-            circles[i].velocity.y *= -1;
-        } else if (circles[i].position.y < circles[i].radius){
-            circles[i].position.y = circles[i].radius; 
-            circles[i].velocity.y *= -1;
-        }
-        if ((circles[i].position.x + circles[i].radius) > screenWidth) {
-            circles[i].position.x = screenWidth - circles[i].radius;
-            circles[i].velocity.x *= -1;
-        } else if (circles[i].position.x < circles[i].radius) {
-            circles[i].position.x = circles[i].radius;
-            circles[i].velocity.x *= -1;
-        }
-    }
+void update() {
+    apply_gravity();
+}
+
+void accelerate(circle_t circle, Vector2 acc) {
+    Vector2Add(circle.acceleration, acc);
+}
+
+void apply_gravity(circle_t circle) {
+    accelerate(circle, gravity); 
+}
+
+void update_circle(int count, circle_t circle, int ldt) {
+    const Vector2 velocity = circle.cur_pos - circle.last_pos;
+    // save the current position
+    circle.last_pos = circle.cur_pos;
+    // perform verlet integration
+    circle.cur_pos = circle.cur_pos + velocity + circle.acceleration * ldt * ldt;
+    circle.acceleration = {0};
+
+    // for (int i = 0; i < count; i++) {
+    //     circles[i].position.x += (circles[i].velocity.x*dt);
+    //     circles[i].position.y += (circles[i].velocity.y*gravity*dt);
+    //     if ((circles[i].position.y + circles[i].radius) > screenHeight) {
+    //         circles[i].position.y = screenHeight - circles[i].radius;
+    //         circles[i].velocity.y *= -1;
+    //     } else if (circles[i].position.y < circles[i].radius){
+    //         circles[i].position.y = circles[i].radius; 
+    //         circles[i].velocity.y *= -1;
+    //     }
+    //     if ((circles[i].position.x + circles[i].radius) > screenWidth) {
+    //         circles[i].position.x = screenWidth - circles[i].radius;
+    //         circles[i].velocity.x *= -1;
+    //     } else if (circles[i].position.x < circles[i].radius) {
+    //         circles[i].position.x = circles[i].radius;
+    //         circles[i].velocity.x *= -1;
+    //     }
+    // }
 }
 
 int main(void) {
@@ -48,8 +68,8 @@ int main(void) {
     // int pos_x = 400;
     // int pos_y = 70;
 
-    circle_t c1 = (circle_t){ .position = (Vector2){.x = 200, .y = 50}, .velocity = (Vector2){ .x = 20.0f, .y = -20.0f }, .radius = 10};
-    circle_t c2 = (circle_t){ .position = (Vector2){.x = 300, .y = 30}, .velocity = (Vector2){ .x = 20.0f, .y = 20.0f }, .radius = 10};
+    circle_t c1 = (circle_t){ .position = (Vector2){.x = 200, .y = 50}, .velocity = (Vector2){ .x = 30.0f, .y = -30.0f }, .radius = 20};
+    circle_t c2 = (circle_t){ .position = (Vector2){.x = 300, .y = 30}, .velocity = (Vector2){ .x = 80.0f, .y = 30.0f }, .radius = 10};
 
     circle_t circles[num_circles] = {c1, c2};
 
